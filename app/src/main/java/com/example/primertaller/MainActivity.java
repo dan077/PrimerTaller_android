7c5707fa96253e6cd9dea0b4bf4e43eabb2218ce
Daniel Vega <danielvegac2000@mail.com>
2021-03-22 17:55:16 -0500

    Login_Btn_verContraseña
    El botón para ver y ocultar contraseña funciona perfectamente.

--- app/src/main/java/com/example/primertaller/MainActivity.java
+++ app/src/main/java/com/example/primertaller/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.primertaller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.PointerIcon;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button verPassword;
+    EditText user,password;
+    Boolean controlVerPassword = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        verPassword = findViewById(R.id.btn_verPassword);
+        user = findViewById(R.id.edit_user);
+        password = findViewById(R.id.edit_password);
+        verPassword.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_verPassword:
+
+                if(controlVerPassword) {
+                    password.setInputType(InputType.TYPE_CLASS_TEXT);
+                    verPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.password_hide, 0, 0, 0);
+                    verPassword.setBackgroundColor(Color.rgb(104,236,142));
+
+                }
+                else {
+                    password.setInputType(InputType.TYPE_CLASS_TEXT |
+                            InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    verPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.password_show, 0);
+                    verPassword.setBackgroundColor(Color.RED);
+
+                }
+
+                //Posiciona el puntero en el ultimo caracter
+                password.setSelection(password.getText().length());
+                
+                //Se cambia el estado de ver/no ver
+                controlVerPassword = !controlVerPassword;
+                break;
+        }
     }
 }
