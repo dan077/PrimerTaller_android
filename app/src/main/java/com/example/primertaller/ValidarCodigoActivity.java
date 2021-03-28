3c56372f078487a2e1b71866e58d92f228b42949
LuisPuelloCP <luiscapupa@gmail.com>
2021-03-28 18:54:41 -0500

    ValidarCodigoActivity
    Se ha creado el diseño y  se ha validado si el usuario esta registrado para enviarle un código de verificación que le permitirá restablecer su contraseña

--- app/src/main/java/com/example/primertaller/ValidarCodigoActivity.java
+++ app/src/main/java/com/example/primertaller/ValidarCodigoActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -16,9 +17,12 @@ import java.util.Random;
 import static java.sql.Types.NULL;
 
 public class ValidarCodigoActivity extends AppCompatActivity implements View.OnClickListener {
+    boolean email;
     int dig5;
     Button envcodigo, validar;
-    EditText codigo;
+    EditText codigo, correo;
+    TextView txgenerado;
+    Funciones adminFunciones = new Funciones(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,8 @@ public class ValidarCodigoActivity extends AppCompatActivity implements View.OnC
         codigo = findViewById(R.id.edtcodigo);
         envcodigo = findViewById(R.id.btnenviarcodigo);
         validar = findViewById(R.id.btnvalidar);
+        correo = findViewById(R.id.edtcorreoveri);
+        txgenerado = findViewById(R.id.txvcode);
 
         envcodigo.setOnClickListener(this);
         validar.setOnClickListener(this);
@@ -35,9 +41,28 @@ public class ValidarCodigoActivity extends AppCompatActivity implements View.OnC
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btnenviarcodigo:
-                Random rnd = new Random();
-                dig5 = rnd.nextInt(90000)+10000; //siempre 5 digitos
-                Toast.makeText(getApplicationContext(), ""+dig5, Toast.LENGTH_LONG).show();
+                if (!correo.getText().toString().isEmpty()){
+                    email = adminFunciones.checkEmail(correo.getText().toString());
+                    if (email == true){
+                        Random rnd = new Random();
+                        dig5 = rnd.nextInt(90000)+10000; //siempre 5 digitos
+                        new AlertDialog.Builder(this)
+                                .setTitle("ENVIADO")
+                                .setMessage("El código a sido generado. Por favor revise SPAM si no lo encuentra en la bandeja principal")
+                                .setPositiveButton(android.R.string.yes, null).show();
+                        txgenerado.setText("El código es: "+ dig5);
+                    }else{
+                        new AlertDialog.Builder(this)
+                                .setTitle("ERROR")
+                                .setMessage("El correo esta mal escrito o no se encuentra en la base de datos")
+                                .setPositiveButton(android.R.string.yes, null).show();
+                    }
+                }else{
+                    new AlertDialog.Builder(this)
+                            .setTitle("ERROR")
+                            .setMessage("Ingrese un correo para enviar un código de verificación")
+                            .setPositiveButton(android.R.string.yes, null).show();
+                }
                 break;
             case R.id.btnvalidar:
                 if (!codigo.getText().toString().isEmpty()){
